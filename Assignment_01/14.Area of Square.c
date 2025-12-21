f894f6c1a97dc5bdc253e4a302752d05fd8d5e36
Shivani Surve <surveshivani99@gmail.com>
2025-12-21 14:42:56 +0530

    Area of Square program

--- Assignment_01/14.Area of Square.c
+++ Assignment_01/14.Area of Square.c
@@ -5,12 +5,13 @@ int main()
 {
     int No, Area;
 
-    printf("\n Enter Side=");
+    printf("\n Enter Side = ");
     scanf("%d",&No);
 
-    Area=No*No;
-    printf("\n Area of Square is: %d", Area);
+    Area = No * No;
+    printf("\n Area of Square is : %d", Area);
 
     getch();
     return 0;
 }
+
