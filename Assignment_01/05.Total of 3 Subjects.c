ba5c58f2d5432c161e862a3d373ed9ecc4c1318a
Shivani Surve <surveshivani99@gmail.com>
2025-12-21 14:40:18 +0530

    Total and percentage calculation code

--- Assignment_01/05.Total of 3 Subjects.c
+++ Assignment_01/05.Total of 3 Subjects.c
@@ -3,22 +3,27 @@
 
 int main()
 {
-    int Physics=0, Chemistry=0, Maths=0;
-    int Total=0;
-    float Percentage=0;
+    int Physics = 0, Chemistry = 0, Maths = 0;
+    int Total = 0;
+    float Percentage = 0;
 
     printf("\n Enter Physics Marks =");
     scanf("%d",&Physics);
-    printf("\n Enter Chemistry Marks=");
+    
+    printf("\n Enter Chemistry Marks = ");
     scanf("%d",&Chemistry);
-    printf("\n Enter Maths Marks=");
+    
+    printf("\n Enter Maths Marks = ");
     scanf("%d",&Maths);
 
-    Total=Physics + Chemistry + Maths;
-    Percentage =(float)Total/3.0;
-    printf("\n Total of 3 Subjects is:%d+%d+%d=%d", Physics, Chemistry, Maths, Total);
+    Total = Physics + Chemistry + Maths;
+    
+    Percentage = (float)Total/3.0;
+    
+    printf("\n Total of 3 Subjects is : %d + %d + %d = %d", Physics, Chemistry, Maths, Total);
     printf("\n Percentage is : %2d", Percentage);
 
     getch();
     return 0;
 }
+
